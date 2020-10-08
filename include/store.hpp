22f913b145920a60fd8f8ad5be39dfd6dcc3ebfb
Borna Bešić <besic.borna@gmail.com>
2020-10-08 23:13:18 +0200

    Add get_cost() to Store

--- include/store.hpp
+++ include/store.hpp
@@ -5,6 +5,7 @@
 
 #include "amount.hpp"
 #include "product.hpp"
+#include "meal.hpp"
 
 using std::unordered_map;
 
@@ -22,5 +23,9 @@ class Store {
         void register_product(const Product &product, const float price, const Amount amount);
         bool has_product(const Product &product);
         const ProductInfo &get_product_info(const Product &product);
+
+        float get_cost(const Product &product, const Amount &amount);
+        float get_cost(const MealComponent &component);
+        float get_cost(const Meal &meal);
 
 };
--- src/store.cpp
+++ src/store.cpp
@@ -1,6 +1,12 @@
 
+#include <numeric>
+#include <utility>
+
+#include "amount.hpp"
 #include "product.hpp"
 #include "store.hpp"
+
+using std::pair;
 
 void Store::register_product(const Product &product, const float price, const Amount amount) {
     this->product_info[product] = {price, amount};
@@ -12,4 +18,32 @@ bool Store::has_product(const Product &product) {
 
 const Store::ProductInfo &Store::get_product_info(const Product &product) {
     return this->product_info[product];
+}
+
+float Store::get_cost(const Product &product, const Amount &amount) {
+    const ProductInfo &info = this->get_product_info(product);
+    float unit_price = info.price / info.amount.value;
+    return unit_price * amount.value;
+}
+
+float Store::get_cost(const MealComponent &component) {
+    return std::accumulate(
+        component.ingredients.begin(),
+        component.ingredients.end(),
+        0.f,
+        [&](const float &sum, const pair<Product, Amount> &p) {
+            return sum + this->get_cost(p.first, p.second);
+        }
+    );
+}
+
+float Store::get_cost(const Meal &meal) {
+    return std::accumulate(
+        meal.components.begin(),
+        meal.components.end(),
+        0.f,
+        [&](const float &sum, const MealComponent &c) {
+            return sum + this->get_cost(c);
+        }
+    );
 }
