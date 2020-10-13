85f152489c2f49fecaf50b83b93d403463591d8d
Borna Bešić <besic.borna@gmail.com>
2020-10-13 13:27:49 +0200

    Add ComponentCountConstraint

--- include/constraint.hpp
+++ include/constraint.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 #include "meal.hpp"
 #include "solver.hpp"
 
 using std::vector;
+using std::string;
 
 // ----------------------------------------- Constraints -----------------------------------------
 class Constraint {
@@ -20,7 +22,18 @@ class MaxBudgetConstraint : public Constraint {
         float max_budget;
 
     public:
-        MaxBudgetConstraint(float max_budget) : max_budget(max_budget) {}
+        MaxBudgetConstraint(float max_budget);
+        bool check(const Context &context, const vector<Meal> &meals) const override;
+
+};
+
+class ComponentCountConstraint : public Constraint {
+    private:
+        string component_name;
+        long int min, max;
+
+    public:
+        ComponentCountConstraint(const string &component_name, long int min = -1, long int max = -1);
         bool check(const Context &context, const vector<Meal> &meals) const override;
 
 };
--- src/constraint.cpp
+++ src/constraint.cpp
@@ -6,10 +6,34 @@
 
 using std::vector;
 
-// ----------------------------------------- Constraints -----------------------------------------
+// ----------------------------------------- Max Budget -----------------------------------------
+
+MaxBudgetConstraint::MaxBudgetConstraint(float max_budget) : max_budget(max_budget) {}
 
 bool MaxBudgetConstraint::check(const Context &context, const vector<Meal> &meals) const {
     float total_cost = context.store.get_cost(meals);
     return total_cost < this->max_budget;
+}
 
+// ----------------------------------------- Count -----------------------------------------
+
+ComponentCountConstraint::ComponentCountConstraint(const string &component_name, long int min, long int max) :
+component_name(component_name), min(min), max(max) {}
+
+bool ComponentCountConstraint::check(const Context &context, const vector<Meal> &meals) const {
+    long int count = 0;
+    for (const auto &meal : meals) {
+        for (const auto &component : meal.components) {
+            if (component.name == this->component_name) ++count;
+        }
+    }
+
+    bool satisfied = true;
+    if (this->min != -1) {
+        satisfied = satisfied && (this->min <= count);
+    }
+    if (this->max != -1) {
+        satisfied = satisfied && (count <= this->max);
+    }
+    return satisfied;
 }
